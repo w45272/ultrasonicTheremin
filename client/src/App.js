409359201d7530a93b74847d8ae53725ee78884e
Will M <w45272@rams.colostate.edu>
2022-11-20 15:01:12 -0700

    API connection?

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+const [profileData, setProfileData] = useState(null)
+
+  function getData() {
+    axios({
+      method: "GET",
+      url:"http:localhost:5000/profile",
+    })
+    .then((response) => {
+      const res =response.data
+      setProfileData(({
+        profile_name: res.name,
+        about_me: res.about}))
+    }).catch((error) => {
+      if (error.response) {
+        console.log(error.response)
+        console.log(error.response.status)
+        console.log(error.response.headers)
+        }
+    })}
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +36,12 @@ function App() {
         >
           Learn React
         </a>
+        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
+        {profileData && <div>
+              <p>Profile name: {profileData.profile_name}</p>
+              <p>About me: {profileData.about_me}</p>
+            </div>
+        }
       </header>
     </div>
   );
