8f835af812e71b4d1abc0979bb001fa3e6c641d4
Will M <w45272@rams.colostate.edu>
2022-11-20 23:06:06 -0700

    rename to NoteSocket

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { WebSocketDemo } from './WebSocket.js'
+import { NoteSocket } from './NoteSocket.js'
 
 function App() {
   return (
     <div className="App">
        <WebSocketDemo/>
+        <NoteSocket/>
     </div>
   );
 }
