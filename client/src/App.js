58b9345ab0751baf138ea71dca617dc6b5b7b938
Will M <w45272@rams.colostate.edu>
2022-11-20 22:19:48 -0700

    comment out old RESTapi

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-const [profileData, setProfileData] = useState(null)
-
-  function getData() {
-    axios({
-      method: "GET",
-      url:"/profile",
-    })
-    .then((response) => {
-      const res =response.data
-      setProfileData(({
-        profile_name: res.name,
-        about_me: res.about}))
-    }).catch((error) => {
-      if (error.response) {
-        console.log(error.response)
-        console.log(error.response.status)
-        console.log(error.response.headers)
-        }
-    })}
+// const [profileData, setProfileData] = useState(null)
+//
+//   function getData() {
+//     axios({
+//       method: "GET",
+//       url:"/profile",
+//     })
+//     .then((response) => {
+//       const res =response.data
+//       setProfileData(({
+//         profile_name: res.name,
+//         about_me: res.about}))
+//     }).catch((error) => {
+//       if (error.response) {
+//         console.log(error.response)
+//         console.log(error.response.status)
+//         console.log(error.response.headers)
+//         }
+//     })}
   return (
     <div className="App">
       <header className="App-header">
@@ -38,12 +38,12 @@ const [profileData, setProfileData] = useState(null)
         >
           Learn React
         </a>
-        <p>To get your profile details: </p><button onClick={getData}>Click me</button>
-        {profileData && <div>
-              <p>Profile name: {profileData.profile_name}</p>
-              <p>About me: {profileData.about_me}</p>
-            </div>
-        }
+        {/* <p>To get your profile details: </p><button onClick={getData}>Click me</button> */}
+        {/* {profileData && <div> */}
+        {/*       <p>Profile name: {profileData.profile_name}</p> */}
+        {/*       <p>About me: {profileData.about_me}</p> */}
+        {/*     </div> */}
+        {/* } */}
       </header>
     </div>
   );
